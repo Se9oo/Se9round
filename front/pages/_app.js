2e5bf106646ec956311f78a3e14c91b20f56b660
shpark <shpark@jness.co.kr>
2021-11-25 09:48:55 +0900

    기본 meta tag 추가

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../public/common/theme';
@@ -6,6 +7,14 @@ import theme from '../public/common/theme';
 const App = ({ Components, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width,
+           maximum-scale=1, minimum-scale=1, user-scalable=no"
+        />
+      </Head>
       <Components {...pageProps} />
     </ChakraProvider>
   );

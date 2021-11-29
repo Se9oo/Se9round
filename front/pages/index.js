5d0ba7979b46a278cadf897137dbbf5604a67f58
park se hyeon <se9oo@kakao.com>
2021-11-29 22:13:26 +0900

    index page > Header, PostList 추가

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import { Box, Heading } from '@chakra-ui/react';
+
+import Header from '../components/Header';
+import PostList from '../components/post/PostList';
+
 const Se9round = () => {
-  return <div>default</div>;
+  return (
+    <Box w="100%" h="100vh" p="0" position="relative">
+      <Header />
+      <Box w={['100%', '100%', '90%', '85%', '85%']} maxW="1200px" m="4rem auto 0" mt="4rem" p="1rem">
+        <Heading as="h2" fontSize="2rem" p="10px">
+          Posts
+        </Heading>
+        <PostList />
+      </Box>
+    </Box>
+  );
 };
 
 export default Se9round;

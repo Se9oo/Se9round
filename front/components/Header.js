fb80f84781031b28e5ce19dd0241c916542c495e
park se hyeon <se9oo@kakao.com>
2021-12-02 01:31:37 +0900

    Header > boxShadow 추가

--- front/components/Header.js
+++ front/components/Header.js
@@ -5,7 +5,7 @@ import { Box } from '@chakra-ui/react';
 const Header = () => {
   return (
     <nav>
-      <Box w="100%" h="3rem" position="fixed" top="0" left="0" bg="white" pb="3rem" mb="3rem" />
+      <Box w="100%" h="3rem" position="fixed" top="0" left="0" bg="white" pb="3rem" mb="3rem" boxShadow="sm" />
     </nav>
   );
 };

e556454ca36d7f80619cbabc8c30fc39dd85d9ff
park se hyeon <se9oo@kakao.com>
2021-12-02 01:33:11 +0900

    PostList > layout 구성 변경 (Heading 추가)

--- front/components/post/PostList.js
+++ front/components/post/PostList.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
-import { Flex } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 
 import PostCard from './PostCard';
 
 const PostList = () => {
   const postList = Array.from({ length: 20 }, () => Array(20).fill(0));
+
   return (
-    <Flex w="100%" flexWrap="wrap">
-      {postList.map((post) => {
-        return <PostCard />;
-      })}
+    <Flex flexDir="column" w="75%">
+      <Heading as="h2" fontSize="2rem" p="10px">
+        Posts
+      </Heading>
+      <Flex flexWrap="wrap">
+        {postList.map((post) => {
+          return <PostCard />;
+        })}
+      </Flex>
     </Flex>
   );
 };

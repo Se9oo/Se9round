d4564facc53cbc585e8fc900f594d2557a8d387a
park se hyeon <se9oo@kakao.com>
2021-12-02 01:33:55 +0900

    index page layout 수정 (TagList 추가)

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Flex } from '@chakra-ui/react';
 
 import Header from '../components/Header';
 import PostList from '../components/post/PostList';
+import TagList from '../components/tag/TagList';
 
 const Se9round = () => {
   return (
     <Box w="100%" h="100vh" p="0" position="relative">
       <Header />
-      <Box w={['100%', '100%', '90%', '85%', '85%']} maxW="1200px" m="4rem auto 0" mt="4rem" p="1rem">
-        <Heading as="h2" fontSize="2rem" p="10px">
-          Posts
-        </Heading>
-        <PostList />
+      <Box w={['100%', '100%', '90%', '85%', '85%']} maxW="1400px" m="4rem auto 0" mt="4rem" p="1rem">
+        <Flex w="100%">
+          <PostList />
+          <TagList />
+        </Flex>
       </Box>
     </Box>
   );

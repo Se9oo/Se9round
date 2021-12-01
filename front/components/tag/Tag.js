99d04fe4a7085c90b349fc0bb1fa2d66dbee0ee0
park se hyeon <se9oo@kakao.com>
2021-12-02 01:33:33 +0900

    TagList, Tag Component 추가

---
+++ front/components/tag/Tag.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Tag as ChakraTag } from '@chakra-ui/react';
+
+const Tag = () => {
+  return (
+    <ChakraTag mr="10px" cursor="pointer">
+      tag
+    </ChakraTag>
+  );
+};
+
+export default Tag;

3cce95a41f4cb7291e82e12e75d7ecc26dacf485
park se hyun <se9oo@kakao.com>
2022-02-04 17:07:34 +0900

    logo 추가

--- front/components/Header.js
+++ front/components/Header.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import router from 'next/router';
 
-import { Flex, Button } from '@chakra-ui/react';
+import { Flex, Button, Image } from '@chakra-ui/react';
 
 const Header = () => {
   const { isAdmin } = useSelector((state) => state.user);
 
+  // 로고 클릭시 홈으로 이동
+  const handleLogo = () => {
+    router.push('/');
+  };
+
+  // 글쓰기 페이지로 이동
   const handleEditPost = () => {
     router.push('/editPost');
   };
@@ -16,7 +22,7 @@ const Header = () => {
       <Flex
         w="100%"
         h="3rem"
-        justifyContent="flex-end"
+        justifyContent="space-between"
         alignItems="center"
         position="fixed"
         top="0"
@@ -25,6 +31,7 @@ const Header = () => {
         boxShadow="sm"
         zIndex="9999"
       >
+        <Image src="/assets/images/se9round_logo.svg" alt="logo" p="1rem" onClick={handleLogo} cursor="pointer" />
         {isAdmin && (
           <Button size="sm" m="0 1rem" onClick={handleEditPost}>
             글쓰기

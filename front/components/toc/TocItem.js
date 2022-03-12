c8843a9ac48d9db4003c28e4da8bdaf5f2603600
park se hyun <se9oo@kakao.com>
2022-03-12 18:38:29 +0900

    게시글 상세 페이지 > TOC 선택한 TOC item 표시 CSS 적용

--- front/components/toc/TocItem.js
+++ front/components/toc/TocItem.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 
-const TocItem = ({ header }) => {
+const TocItem = ({ header, selectedTocItem, handleTocItem }) => {
   const { href, text, count } = header;
 
   return (
     <Box ml={`${count / 2}rem`}>
-      <a href={href}>{text}</a>
+      <Text
+        color={selectedTocItem === href ? 'brown' : 'gray.500'}
+        fontWeight={selectedTocItem === href ? '600' : '500'}
+        transform={selectedTocItem === href ? 'scale(1.05)' : 'scale(1)'}
+        transition="all .1s ease"
+        _hover={selectedTocItem !== href && { color: 'black' }}
+      >
+        <a href={href} onClick={(e) => handleTocItem(e, href)}>
+          {text}
+        </a>
+      </Text>
     </Box>
   );
 };
--- front/components/toc/index.js
+++ front/components/toc/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Box } from '@chakra-ui/react';
 
@@ -6,7 +6,15 @@ import { getMarkdownHeader } from '../../util/common';
 import TocItem from './TocItem';
 
 const TOC = ({ title, contents }) => {
+  const [selectedTocItem, setSelectedTocItem] = useState(null);
   const markdownHeaders = getMarkdownHeader(title, contents);
+
+  const handleTocItem = (e, href) => {
+    e.preventDefault();
+
+    setSelectedTocItem(href);
+    document.location.href = href;
+  };
 
   return (
     <Box
@@ -19,7 +27,14 @@ const TOC = ({ title, contents }) => {
       p="1rem"
     >
       {markdownHeaders.map((header, idx) => {
-        return <TocItem key={`${header.text}_${idx}`} header={header} />;
+        return (
+          <TocItem
+            key={`${header.text}_${idx}`}
+            header={header}
+            selectedTocItem={selectedTocItem}
+            handleTocItem={handleTocItem}
+          />
+        );
       })}
     </Box>
   );

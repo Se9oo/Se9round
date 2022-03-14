117203455806eb8ea0812cd993a7d8939a70f056
park se hyun <se9oo@kakao.com>
2022-03-14 17:36:02 +0900

    footer > github icon 및 링크 추가

--- front/components/footer/index.js
+++ front/components/footer/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Img, Text } from '@chakra-ui/react';
 
 const Footer = () => {
+  const handleGithubIcon = () => {
+    window.open('https://github.com/Se9oo', '_blank');
+  };
   return (
     <footer>
-      <Flex w="100%" h="4rem" justifyContent="center" alignItems="center" bgColor="brown">
-        <Text color="white" fontWeight="700">
+      <Flex w="100%" h="6rem" flexDir="column" justifyContent="center" alignItems="center" bgColor="white">
+        <Text color="gray.400" fontWeight="700" mb=".5rem">
           Copyright &copy; 2022 se9oo All Rights Reserved.
         </Text>
+        <Img src="/assets/images/github.svg" alt="github-logo" w="1.8rem" cursor="pointer" onClick={handleGithubIcon} />
       </Flex>
     </footer>
   );

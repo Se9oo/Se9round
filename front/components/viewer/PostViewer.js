d674d58940096af3caea368f3129cffcc60f5fc9
park se hyun <se9oo@kakao.com>
2022-03-15 16:32:47 +0900

    게시글 상세 페이지 > 등록일자 추가 및 style 변경

--- front/components/viewer/PostViewer.js
+++ front/components/viewer/PostViewer.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import { Box, Heading, Tag, TagLabel } from '@chakra-ui/react';
+import { Box, Flex, Heading, Tag, TagLabel, Text } from '@chakra-ui/react';
+import { getDateFormatToKor } from '../../util/common';
 
 const DynamicPostViewer = dynamic(() => import('./Viewer'), { ssr: false });
 
-const PostViewer = ({ title, contents, tags }) => {
+const PostViewer = ({ title, contents, tags, reg_dt }) => {
   return (
-    <Box w="100%" bg="white" borderRadius="1rem" m="0 auto 5rem auto" p="1rem">
-      <Heading as="h1" fontSize="3rem" mb="2rem">
-        {title}
-      </Heading>
-      <Box mb="2rem">
-        {tags &&
-          tags.map((tag, idx) => (
-            <Tag
-              key={`${tag}_${idx}`}
-              fontSize="1.2rem"
-              m=".1rem .5rem .1rem 0"
-              cursor="pointer"
-              _hover={{ bg: 'rgba(226, 232, 240, .3)' }}
-            >
-              <TagLabel>{tag}</TagLabel>
-            </Tag>
-          ))}
-      </Box>
+    <Box w="100%" bg="white" m="0 auto 5rem auto" p="3rem">
+      <Flex w="100%" flexDir="column" justifyContent="center" alignItems="center">
+        <Heading as="h1" fontSize="3rem" mb="2rem">
+          {title}
+        </Heading>
+        <Box mb="2rem">
+          {tags &&
+            tags.map((tag, idx) => (
+              <Tag
+                key={`${tag}_${idx}`}
+                fontSize="1.2rem"
+                m=".1rem .5rem .1rem 0"
+                bg="brown"
+                color="white"
+                cursor="pointer"
+                _hover={{ bg: 'gray.200' }}
+              >
+                <TagLabel>{tag}</TagLabel>
+              </Tag>
+            ))}
+        </Box>
+        <Text color="gray.400" mb="2rem">
+          {getDateFormatToKor(reg_dt)}
+        </Text>
+      </Flex>
       <DynamicPostViewer contents={contents} />
     </Box>
   );

84ad4ba2ce72795eb5b3edd6b8c080fc8076c5ea
park se hyun <se9oo@kakao.com>
2022-03-16 18:02:18 +0900

    user router > request body check middleware 추가

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,28 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const { verifyTokens, createAccessToken, createRefreshToken } = require('../middlewares/auth');
+const { requestBodyCheck } = require('../middlewares/paramCheck');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 // 관리자 로그인
-router.post('/api/admin-login', (req, res) => {
-  if (!req.body) {
-    res.status(500).json('empty request');
+router.post('/api/admin-login', requestBodyCheck, (req, res) => {
+  const { key } = req.body.data;
+
+  if (key === process.env.LOGIN_KEY) {
+    const payload = { admin: 'admin' };
+    const accessToken = createAccessToken(payload);
+    const refreshToken = createRefreshToken();
+
+    res.cookie('user', accessToken, { httpOnly: true, overwrite: true });
+    res.cookie('refresh', refreshToken, { httpOnly: true, overwrite: true });
+    res.status(200).json('success');
   } else {
-    const { key } = req.body.data;
-
-    if (key === process.env.LOGIN_KEY) {
-      const payload = { admin: 'admin' };
-      const accessToken = createAccessToken(payload);
-      const refreshToken = createRefreshToken();
-
-      res.cookie('user', accessToken, { httpOnly: true, overwrite: true });
-      res.cookie('refresh', refreshToken, { httpOnly: true, overwrite: true });
-      res.status(200).json('success');
-    } else {
-      res.status(401).json('로그인 실패');
-    }
+    res.status(401).json('로그인 실패');
   }
 });
 

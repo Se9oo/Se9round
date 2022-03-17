6e32eef18554ef126b769e78fc102b1db79052f1
park se hyun <se9oo@kakao.com>
2022-03-17 17:44:27 +0900

    viewer > contents가 변할 경우 viewer 렌더링 처리

--- front/components/viewer/Viewer.js
+++ front/components/viewer/Viewer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { Viewer as TuiViewer } from '@toast-ui/react-editor';
 import codeSyntaxHightlight from '@toast-ui/editor-plugin-code-syntax-highlight';
@@ -7,6 +7,7 @@ import hljs from 'highlight.js';
 import 'highlight.js/styles/atom-one-dark.css';
 
 const Viewer = ({ contents }) => {
+  const viewerRef = useRef();
   // header tag에 id 설정
   useEffect(() => {
     const tags = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
@@ -19,7 +20,12 @@ const Viewer = ({ contents }) => {
     });
   }, []);
 
-  return <TuiViewer initialValue={contents} plugins={[[codeSyntaxHightlight, { hljs }]]} />;
+  // contents가 바뀐경우 viewer에 세팅
+  useEffect(() => {
+    viewerRef.current.getInstance().setMarkdown(contents);
+  }, [contents]);
+
+  return <TuiViewer initialValue={contents} ref={viewerRef} plugins={[[codeSyntaxHightlight, { hljs }]]} />;
 };
 
 export default Viewer;

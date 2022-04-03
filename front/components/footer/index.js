c0b6ea93371456b1145ff21c213292d843d1b7be
park se hyun <se9oo@kakao.com>
2022-04-03 17:05:16 +0900

    footer > font size 수정, text-align 속성 추가

--- front/components/footer/index.js
+++ front/components/footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <footer>
       <Flex w="100%" h="6rem" flexDir="column" justifyContent="center" alignItems="center" bgColor="white">
-        <Text color="gray.400" fontWeight="700" mb=".5rem">
+        <Text fontSize="1rem" color="gray.400" fontWeight="700" mb=".5rem" textAlign="center">
           Copyright &copy; 2022 se9oo All Rights Reserved.
         </Text>
         <Img src="/assets/images/github.svg" alt="github-logo" w="1.8rem" cursor="pointer" onClick={handleGithubIcon} />

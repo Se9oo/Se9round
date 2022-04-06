40960c76aadb10f58d41107408baba18556a9fd5
park se hyun <se9oo@kakao.com>
2022-04-06 19:36:13 +0900

    generate-sitemap.sh 경로 수정

--- front/script/build-app.sh
+++ front/script/build-app.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 cd /home/Se9round/front
 
 npx pm2 kill
--- front/script/generate-sitemap.sh
+++ front/script/generate-sitemap.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env sh
 cd /home/Se9round/front/public
 
 rm -rf sitemap.xml
 
-/root/.nvm/versions/node/v16.14.2/bin/node robots.js
-/root/.nvm/versions/node/v16.14.2/bin/node sitemap.js
+/root/.nvm/versions/node/v16.14.2/bin/node /home/Se9round/front/script/robots.js
+/root/.nvm/versions/node/v16.14.2/bin/node /home/Se9round/front/script/sitemap.js

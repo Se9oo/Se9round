75f50564360626d8cc2546f50b7160ba095cce2b
park se hyun <se9oo@kakao.com>
2022-04-06 16:59:56 +0900

    generate site map 수정, build script 추가

---
+++ front/script/build-app.sh
@@ -0,0 +1,10 @@
+cd /home/Se9round/front
+
+npx pm2 kill
+
+npm run build
+
+npx pm2 start npm -- start
+
+curl http://google.com/ping?sitemap=https://se9round.dev/sitemap.xml
+
--- front/script/generate-sitemap.sh
+++ front/script/generate-sitemap.sh
@@ -7,5 +7,3 @@ cd script
 
 node robots.js
 node sitemap.js
-
-curl http://google.com/ping?sitemap=https://se9round.dev/sitemap.xml

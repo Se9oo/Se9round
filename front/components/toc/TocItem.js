b6e9d8f2fd914e78845d7dfc0eb94f14703a982f
park se hyun <se9oo@kakao.com>
2022-04-08 15:44:32 +0900

    TOC > font size 조정, tag 별 들여쓰기 값 수정

--- front/components/toc/TocItem.js
+++ front/components/toc/TocItem.js
@@ -6,14 +6,15 @@ const TocItem = ({ header, selectedTocItem, handleTocItem }) => {
   const { href, text, count } = header;
 
   return (
-    <Box ml={`${count / 2}rem`}>
+    <Box ml={`${count}rem`}>
       <Text
         color={selectedTocItem === href ? 'brown' : 'gray.500'}
-        fontSize=".9rem"
+        fontSize=".8rem"
         fontWeight={selectedTocItem === href ? '600' : '500'}
         transform={selectedTocItem === href ? 'scale(1.05)' : 'scale(1)'}
         transition="all .1s ease"
         _hover={selectedTocItem !== href && { color: 'black' }}
+        wordBreak="break-all"
       >
         <a href={href} onClick={(e) => handleTocItem(e, href)}>
           {text}
